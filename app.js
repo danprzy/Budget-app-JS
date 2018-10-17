4b0a35ab0f3d8b9a414b595d27378b571f3f98eb
Danuta Przydrozna <danprzy@gmail.com>
2018-10-17 21:25:31 +0200

    Add reading input data

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-
 //Budget controller
 var budgetController = (function() {
 
- // some code
+    // some code
 
 })();
 
@@ -10,7 +9,27 @@ var budgetController = (function() {
 // UI controller
 var UIControler = (function() {
 
-    // some code
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                // will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
 
 })();
 
@@ -18,22 +37,25 @@ var UIControler = (function() {
 // Global app controler
 var controler = (function(budgetCtrl, UICtrl) {
 
-var ctrlAddItem = function() {
-console.log('działa');
-	// 1. get the field input data
- 	// 2. add item to the budget controller
- 	// 3. add the item to UI
- 	// 4. calculate budget
- 	// 5 display the budget on the UI
+    var DOM = UICtrl.getDOMstrings();
 
-}
+    var ctrlAddItem = function() {
 
- document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+        // 1. get the field input data
+        var input = UIControler.getInput();
+        console.log(input);
+        // 2. add item to the budget controller
+        // 3. add the item to UI
+        // 4. calculate budget
+        // 5 display the budget on the UI
+    };
 
-document.addEventListener('keypress', function(event) {
- 	if (event.keyCode === 13 || event.which === 13) { // which for older browser
- 		ctrlAddItem();
- 	}
- });
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) { // which for older browser
+            ctrlAddItem();
+        }
+    });
 
 })(budgetController, UIControler);
