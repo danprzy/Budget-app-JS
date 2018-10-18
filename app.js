bd4e876b1882a12be86181169b44db9f6e564bb3
Danuta Przydrozna <danprzy@gmail.com>
2018-10-18 23:52:22 +0200

    Add new item to budget controller and UI

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ var budgetController = (function() {
         this.value = value;
     };
 
-   // var allExpenses = [];
-   // var allIncomes = [];
+    // var allExpenses = [];
+    // var allIncomes = [];
     // var totalExpenses = 0;
-   // var totalIncomes = 0;
+    // var totalIncomes = 0;
 
     var data = {
         allItems: {
@@ -27,7 +27,38 @@ var budgetController = (function() {
             exp: 0,
             inc: 0
         }
-    }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            // create new id
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+                // ID = last ID + 1   
+            } else {
+                ID = 0;
+            }
+
+
+            //create new item
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            // push into data structure
+            data.allItems[type].push(newItem);
+
+            // return new element
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+    };
 
 })();
 
@@ -39,7 +70,9 @@ var UIControler = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     };
 
     return {
@@ -50,6 +83,25 @@ var UIControler = (function() {
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value
             };
+        },
+
+        addListItem: function(obj, type) {
+            var html, newHtml;
+            // create HTML string with placeholder text
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+      html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+        } else if (type === 'exp') {
+            element.DOMstrings.expensesContainer;
+        html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+}
+            // replace the placeholder text with some actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+            
+            // Insert the HTM<L into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
         getDOMstrings: function() {
@@ -77,12 +129,15 @@ var controler = (function(budgetCtrl, UICtrl) {
 
 
     var ctrlAddItem = function() {
-
+        var input, newItem;
         // 1. get the field input data
-        var input = UIControler.getInput();
-        console.log(input); 
+        input = UIControler.getInput();
+
         // 2. add item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
         // 3. add the item to UI
+        UIControler.addListItem(newItem, input.type);
         // 4. calculate budget
         // 5 display the budget on the UI
     };
